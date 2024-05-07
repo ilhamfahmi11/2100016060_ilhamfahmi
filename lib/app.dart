d268ce91e3da8b9908d9ec2b0d4b7cc8cbebb5b4
ilhamfahmi11 <ilham.fahmi011@gmail.com>
2024-05-07 22:23:00 +0700

    commit

---
+++ lib/app.dart
@@ -0,0 +1,106 @@
+// Copyright 2018-present the Flutter authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import 'package:flutter/material.dart';
+import 'colors.dart';
+import 'supplemental/cut_corners_border.dart';
+
+import 'home.dart';
+import 'login.dart';
+
+// TODO: Build a Shrine Theme (103)
+final ThemeData _kShrineTheme = _buildShrineTheme();
+
+// TODO: Build a Shrine Text Theme (103)
+TextTheme _buildShrineTextTheme(TextTheme base) {
+  return base
+      .copyWith(
+        headlineSmall: base.headlineSmall!.copyWith(
+          fontWeight: FontWeight.w500,
+        ),
+        titleLarge: base.titleLarge!.copyWith(
+          fontSize: 18.0,
+        ),
+        bodySmall: base.bodySmall!.copyWith(
+          fontWeight: FontWeight.w400,
+          fontSize: 14.0,
+        ),
+        bodyLarge: base.bodyLarge!.copyWith(
+          fontWeight: FontWeight.w500,
+          fontSize: 16.0,
+        ),
+      )
+      .apply(
+        fontFamily: 'Rubik',
+        displayColor: kShrineYellow900,
+        bodyColor: kShrineYellow900,
+      );
+}
+
+ThemeData _buildShrineTheme() {
+  final ThemeData base = ThemeData.light();
+  return base.copyWith(
+    colorScheme: base.colorScheme.copyWith(
+      primary: kShrineTosca,
+      onPrimary: kShrineTosca,
+      secondary: kShrineTosca,
+      error: kShrineTosca,
+    ),
+    // TODO: Add the text themes (103)
+    scaffoldBackgroundColor: kShrineBackgroundGrey,
+    textSelectionTheme: const TextSelectionThemeData(
+      selectionColor: kShrineTosca,
+    ),
+    // TODO: Add the icon themes (103)
+    // TODO: Decorate the inputs (103)
+    inputDecorationTheme: const InputDecorationTheme(
+      border: CutCornersBorder(),
+      focusedBorder: CutCornersBorder(
+        borderSide: BorderSide(
+          width: 2.0,
+          color: kShrineTosca,
+        ),
+      ),
+      floatingLabelStyle: TextStyle(
+        color: kShrineTosca,
+      ),
+    ),
+  );
+}
+
+// TODO: Convert ShrineApp to stateful widget (104)
+class ShrineApp extends StatelessWidget {
+  const ShrineApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shrine',
+      initialRoute: '/login',
+      routes: {
+        '/login': (BuildContext context) => const LoginPage(),
+        // TODO: Change to a Backdrop with a HomePage frontLayer (104)
+        '/': (BuildContext context) => const HomePage(),
+        // TODO: Make currentCategory field take _currentCategory (104)
+        // TODO: Pass _currentCategory for frontLayer (104)
+        // TODO: Change backLayer field value to CategoryMenuPage (104)
+      },
+      // TODO: Add a theme (103)
+      theme: _kShrineTheme, // New code
+    );
+  }
+}
+
+// TODO: Build a Shrine Theme (103)
+// TODO: Build a Shrine Text Theme (103)
---
+++ lib/colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+const kShrineTosca = Color(0xFF1ABC9C);
+const kShrineTurquoise100 = Color(0xFFB2EBF2);
+const kShrineTurquoise300 = Color(0xFF4DD0E1);
+const kShrineTurquoise400 = Color(0xFF26A69A);
+
+const kShrineYellow900 = Color(0xFFC79100);
+
+const kShrineGreen = Color(0xFF008000);
+
+const kShrineOrange = Color(0xFFFFA500);
+const kShrineBackgroundGrey = Colors.grey;
